82ebb249082bb1087ac3c38fb8bf263948c40641
Ahmed-P-Mostafa <Ahmed-P-Mostafa@users.noreply.github.com>
2020-10-21 02:13:46 +0200

    added memory dialog implemented recyclerView with adapter and memory_item.xml edited memory entity to add locationName:String

--- app/src/main/java/com/example/memoriesproject/database/MemoryEntity.kt
+++ app/src/main/java/com/example/memoriesproject/database/MemoryEntity.kt
@@ -11,5 +11,6 @@ data class MemoryEntity(@PrimaryKey(autoGenerate = true)
                         val id :Int?=null,val title:String?=null,
                         val description:String?=null,
                         val imagesArray:ArrayList<Uri>?=null,
-                        val Position:Location?=null) {
+                        val Position:Location?=null,
+                        val locationName:String) {
 }
